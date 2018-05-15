053dd95e863c52744b110e726953f69c663dcb03
eugenia cadavid <eumar-8@hotmail.com>
2018-05-15 10:20:59 +0200

    Rails new with frontend and form gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'bookings/new'
-
-  get 'bookings/create'
-
-  get 'bookings/destroy'
-
-  get 'pets/index'
-
-  get 'pets/create'
-
-  get 'pets/new'
-
-  get 'pets/show'
-
-  get 'pets/edit'
-
-  get 'pets/update'
-
-  get 'pets/destroy'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/destroy'
 
   root to: 'pages#home'
+  get 'design', to: 'pages#design'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create, :edit, :destroy ]
   resources :pets do
