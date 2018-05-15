59161b893dbe6a78bdad81ef4838f5dc4b7c2036
susott <37674058+susott@users.noreply.github.com>
2018-05-15 18:26:39 +0200

    created a pet-show and added the new booking functionality. Also added a user dashboard

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :pet
+  belongs_to :user
 
   validates :date_from, presence: true
   validates :date_to, presence: true
-  validates :location, presence: true
 end
