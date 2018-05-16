78d5bd3a4a5943970d588ca62532c21e858ecd25
susott <37674058+susott@users.noreply.github.com>
2018-05-16 17:10:37 +0200

    added photo to seed, changed title, added link to new pet on homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,19 +10,26 @@ User.destroy_all
 user = User.create(email: "batman@dc.com", password: "123456", name: "Batman", location: "Rødovre")
 pet = Pet.new(name: "Big Pete", price: "500", species: "elephant", age: 43, description: "Super playful elephant")
 pet.user = user
+pet.photo = Rails.root.join("app/assets/images/elephant.jpg").open
 pet.save
+
+# pet.photo = Picture.create(image_path: "elephant.jpg")
 
 user1 = User.create(email: "emilio@dc.com", password: "123456", name: "emilio", location: "Rødovre")
 pet1 = Pet.new(name: "tommy", price: "150", species: "dog", age: 3, description: "Super playful dog")
+pet.photo = Rails.root.join("app/assets/images/dogs-background.jpg").open
 pet1.user = user1
 pet1.save
 
 user2 = User.create(email: "eugenia@dc.com", password: "123456", name: "eugenia", location: "Bogota")
 pet2 = Pet.new(name: "michifu", price: "200", species: "cat", age: 1, description: "Super playful cat")
+pet.photo = Rails.root.join("app/assets/images/cat.jpeg").open
 pet2.user = user2
+
 pet2.save
 
 user3 = User.create(email: "eugenia@dc.com", password: "123456", name: "eugenia", location: "Bogota")
 pet3 = Pet.new(name: "caremico", species: "fish", age: 1, description: "Super ugly but nice")
+pet.photo = Rails.root.join("app/assets/images/goldfish.jpeg").open
 pet3.user = user2
 pet3.save
