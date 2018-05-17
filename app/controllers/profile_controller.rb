62dcf59fb7945932e41bc12e259a8cdaef47a3ce
eugenia cadavid <eumar-8@hotmail.com>
2018-05-17 17:06:32 +0200

    chages in user

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,11 +6,14 @@ class ProfileController < ApplicationController
   end
 
   def update
+    # raise
     location = params[:user][:location]
     name = params[:user][:name]
-    photo
+    photo = params[:user][:photo]
+
     current_user.location = location
     current_user.name = name
+    current_user.photo = photo
     current_user.save!
     redirect_to(profile_path(current_user))
   end
