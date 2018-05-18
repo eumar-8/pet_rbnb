67d151ef471927417fe59997a6acd5cbd8347a65
eugenia cadavid <eumar-8@hotmail.com>
2018-05-18 16:37:56 +0200

    fix some thing in each pege

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
   mount_uploader :photo, PhotoUploader
 
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
 end
